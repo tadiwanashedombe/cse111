4fbf60d794e7bef0f20f3636379596798a10badd
tadiwanashedombe <dombeseant@gmail.com>
2026-05-10 00:09:59 +0200

    moved test.py to prractice

--- test.py
+++
@@ -1,5 +0,0 @@
-city_name = "Accra"
-# Open a text file named cities.txt in append mode.
-with open("volume.txt", "at") as cities_file:
-  # Print a city's name and information to the file.
-  print(city_name, file=cities_file)
